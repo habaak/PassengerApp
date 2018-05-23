d1947ac9c290cf056a1bb7048e27f329f6f62fc9
habaak <joohn728@gmail.com>
2018-05-23 10:47:05 +0900

    ADD first Ionic project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
+import { BusPage } from '../pages/bus/bus';
+import { MyPage } from '../pages/my/my';
+import { MapPage } from '../pages/map/map';
+import { LoginPage } from '../pages/login/login'
+import { SignupPage } from '../pages/signup/signup'
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    MapPage,
+    MyPage,
+    BusPage,
+    LoginPage,
+    SignupPage,
     TabsPage
   ],
   imports: [
@@ -26,9 +33,12 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
+    MapPage,
+    MyPage,
+    BusPage,
+    LoginPage,
+    SignupPage,
     TabsPage
   ],
   providers: [
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
+import { BusPage } from '../bus/bus';
+import { MyPage } from '../my/my';
 import { HomePage } from '../home/home';
+import { MapPage } from '../map/map';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -10,8 +11,10 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = MapPage;
+  tab3Root = BusPage;
+  tab4Root = MyPage;
+  
 
   constructor() {
 
